3d48d73251afa55a24ca46c091f59a4229463df0
Spencer Fletcher <44955744+cidarid@users.noreply.github.com>
2026-03-20 14:56:00 -0700

    Adding robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import {defineConfig} from 'astro/config';
 
 import svelte from "@astrojs/svelte";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte()]
+	integrations: [svelte(), sitemap({
+		filter: (page) => !page.includes('/ja') && !page.includes('/marquee'),
+	})]
 });
